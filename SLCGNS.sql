cf07fa1865d1d14a2d605ff199ccddebfb52dbd6
Brendan Bautista <105677945+brendanbautista@users.noreply.github.com>
2024-11-11 09:29:26 -0700

    Update SLCGNS.sql

--- SLCGNS.sql
+++ SLCGNS.sql
@@ -2,8 +2,11 @@ SELECT
 
 svc.invoice_ref
 ,s.shpmnt_ref
-,(select CAST(19+INT((e.date_stat) / 100000000000000000) AS CHAR(2)) || TRANSLATE('YE-MN-DA', DIGITS(e.date_stat),'xxYEMNDAxHRUTSC') from export.stat_item e where e.invoice_ref = s.shpmnt_ref and e.stat_code = 'LKI' and e.remarks_stat like ('%' || svc.invoice_ref || '%') fetch first row only) as LKI
-
+,(select max(CAST(19+INT((e.date_stat) / 100000000000000000) AS CHAR(2)) || TRANSLATE('YE-MN-DA', DIGITS(e.date_stat),'xxYEMNDAxHRUTSC')) from export.stat_item e where e.invoice_ref = s.shpmnt_ref and e.stat_code = 'LKI' and e.remarks_stat like ('%' || svc.invoice_ref || '%') fetch first row only) as LKI
+,(select max(CAST(19+INT((e.date_stat) / 100000000000000000) AS CHAR(2)) || TRANSLATE('YE-MN-DA', DIGITS(e.date_stat),'xxYEMNDAxHRUTSC')) from export.stat_item e where e.invoice_ref = s.shpmnt_ref and e.stat_code = 'DSN') as DSN
+,(select max(CAST(19+INT((e.date_stat) / 100000000000000000) AS CHAR(2)) || TRANSLATE('YE-MN-DA', DIGITS(e.date_stat),'xxYEMNDAxHRUTSC')) from export.stat_item e where e.invoice_ref = s.shpmnt_ref and e.stat_code = 'BLP') as BLP
+,(select max(CAST(19+INT((e.date_stat) / 100000000000000000) AS CHAR(2)) || TRANSLATE('YE-MN-DA', DIGITS(e.date_stat),'xxYEMNDAxHRUTSC')) from export.stat_item e where e.invoice_ref = s.shpmnt_ref and e.stat_code = 'SMP') as SMP
+,(select max(CAST(19+INT((e.date_stat) / 100000000000000000) AS CHAR(2)) || TRANSLATE('YE-MN-DA', DIGITS(e.date_stat),'xxYEMNDAxHRUTSC')) from export.stat_item e where e.invoice_ref = s.shpmnt_ref and e.stat_code in ('FRD','FND')) as FRD
 
 FROM export.SVC_PROVIDER_ITM SVC
 
@@ -14,3 +17,4 @@ WHERE SVC.SVC_PROVIDER_ID = 'G0431613'
 AND SVC.Date_Create > 123120100000000000
 AND SVC.TRAN_UPDATE not in ('130')
 AND S.PORT_ORIGIN not in ('LAS')
+AND left(s.shpmnt_ref, 1) in ('H','4')
