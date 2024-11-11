9b7f568d44a828d83245a4578893d29df8809e50
Brendan Bautista <105677945+brendanbautista@users.noreply.github.com>
2024-11-11 14:29:29 -0700

    Update SLCGNS.sql

--- SLCGNS.sql
+++ SLCGNS.sql
@@ -1,20 +1,97 @@
-SELECT 
-
+WITH MAIN AS (
+SELECT
 svc.invoice_ref
 ,s.shpmnt_ref
-,(select max(CAST(19+INT((e.date_stat) / 100000000000000000) AS CHAR(2)) || TRANSLATE('YE-MN-DA', DIGITS(e.date_stat),'xxYEMNDAxHRUTSC')) from export.stat_item e where e.invoice_ref = s.shpmnt_ref and e.stat_code = 'LKI' and e.remarks_stat like ('%' || svc.invoice_ref || '%') fetch first row only) as LKI
-,(select max(CAST(19+INT((e.date_stat) / 100000000000000000) AS CHAR(2)) || TRANSLATE('YE-MN-DA', DIGITS(e.date_stat),'xxYEMNDAxHRUTSC')) from export.stat_item e where e.invoice_ref = s.shpmnt_ref and e.stat_code = 'DSN') as DSN
-,(select max(CAST(19+INT((e.date_stat) / 100000000000000000) AS CHAR(2)) || TRANSLATE('YE-MN-DA', DIGITS(e.date_stat),'xxYEMNDAxHRUTSC')) from export.stat_item e where e.invoice_ref = s.shpmnt_ref and e.stat_code = 'BLP') as BLP
-,(select max(CAST(19+INT((e.date_stat) / 100000000000000000) AS CHAR(2)) || TRANSLATE('YE-MN-DA', DIGITS(e.date_stat),'xxYEMNDAxHRUTSC')) from export.stat_item e where e.invoice_ref = s.shpmnt_ref and e.stat_code = 'SMP') as SMP
-,(select max(CAST(19+INT((e.date_stat) / 100000000000000000) AS CHAR(2)) || TRANSLATE('YE-MN-DA', DIGITS(e.date_stat),'xxYEMNDAxHRUTSC')) from export.stat_item e where e.invoice_ref = s.shpmnt_ref and e.stat_code in ('FRD','FND')) as FRD
-
+,o.onhand_ref
+,s.consol_ref
+ 
 FROM export.SVC_PROVIDER_ITM SVC
-
 Left Outer Join export.shpmnt_hdr s
         On svc.invoice_ref = s.consol_ref
-
+LEFT OUTER JOIN EXPORT.ONHAND_HDR O
+        ON O.SHPMNT_REF = S.SHPMNT_REF        
 WHERE SVC.SVC_PROVIDER_ID = 'G0431613'
 AND SVC.Date_Create > 123120100000000000
 AND SVC.TRAN_UPDATE not in ('130')
 AND S.PORT_ORIGIN not in ('LAS')
 AND left(s.shpmnt_ref, 1) in ('H','4')
+
+)
+,
+HOUSE_EVENTS AS(
+        SELECT
+        M.SHPMNT_REF
+        ,E.SMP
+        ,E.DSN
+        ,E.LKI
+        ,E.FRD
+        ,E.BLP
+        FROM
+                MAIN M
+                INNER JOIN LATERAL(
+                        SELECT
+                                M.SHPMNT_REF
+                                ,LAST_VALUE(CASE WHEN E.STAT_CODE IN ('SMP') THEN E.DATE_STAT END , 'IGNORE NULLS')
+                                        OVER (PARTITION BY E.INVOICE_REF ORDER BY E.UPDATE_DATE, E.DATE_CREATE, E.ORDAT_SEQUENCE ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+                                        AS SMP                        
+                                ,LAST_VALUE(CASE WHEN E.STAT_CODE IN ('DSN') THEN E.DATE_STAT END , 'IGNORE NULLS')
+                                        OVER (PARTITION BY E.INVOICE_REF ORDER BY E.UPDATE_DATE, E.DATE_CREATE, E.ORDAT_SEQUENCE ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+                                        AS DSN                                
+                                ,LAST_VALUE(CASE WHEN E.STAT_CODE IN ('LKI') THEN E.DATE_STAT END , 'IGNORE NULLS')
+                                        OVER (PARTITION BY E.INVOICE_REF ORDER BY E.UPDATE_DATE, E.DATE_CREATE, E.ORDAT_SEQUENCE ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+                                        AS LKI 
+                                ,LAST_VALUE(CASE WHEN E.STAT_CODE IN ('FRD','FND') THEN E.DATE_STAT END , 'IGNORE NULLS')
+                                        OVER (PARTITION BY E.INVOICE_REF ORDER BY E.UPDATE_DATE, E.DATE_CREATE, E.ORDAT_SEQUENCE ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+                                        AS FRD   
+                                ,LAST_VALUE(CASE WHEN E.STAT_CODE IN ('BLP') THEN E.DATE_STAT END , 'IGNORE NULLS')
+                                        OVER (PARTITION BY E.INVOICE_REF ORDER BY E.UPDATE_DATE, E.DATE_CREATE, E.ORDAT_SEQUENCE ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+                                        AS BLP                                                                                                                
+                               ,ROW_NUMBER() OVER(PARTITION BY E.INVOICE_REF) AS RW
+                        FROM
+                                EXPORT.STAT_ITEM E
+                        WHERE
+                                E.INVOICE_REF IN (M.SHPMNT_REF, M.CONSOL_REF,M.ONHAND_REF)
+                                AND E.STAT_CODE IN ('SMP','DSN','LKI','FRD','FND','BLP')
+                )E ON E.RW = 1
+)
+ 
+ 
+SELECT
+ 
+DISTINCT
+ 
+M.SHPMNT_REF
+,M.CONSOL_REF
+,MAX(CAST(19+INT((HE.SMP) / 
+                100000000000000000) AS CHAR(2)) || 
+                TRANSLATE('YE-MN-DA', 
+                DIGITS(HE.SMP),
+                'xxYEMNDAxHRUTSC')) AS SMP_DATE
+,MAX(CAST(19+INT((HE.DSN) / 
+                100000000000000000) AS CHAR(2)) || 
+                TRANSLATE('YE-MN-DA', 
+                DIGITS(HE.DSN),
+                'xxYEMNDAxHRUTSC')) AS DSN_DATE
+,MAX(CAST(19+INT((HE.LKI) / 
+                100000000000000000) AS CHAR(2)) || 
+                TRANSLATE('YE-MN-DA', 
+                DIGITS(HE.LKI),
+                'xxYEMNDAxHRUTSC')) AS LKI_DATE
+,MAX(CAST(19+INT((HE.FRD) / 
+                100000000000000000) AS CHAR(2)) || 
+                TRANSLATE('YE-MN-DA', 
+                DIGITS(HE.FRD),
+                'xxYEMNDAxHRUTSC')) AS FRD_DATE
+,MAX(CAST(19+INT((HE.BLP) / 
+                100000000000000000) AS CHAR(2)) || 
+                TRANSLATE('YE-MN-DA', 
+                DIGITS(HE.BLP),
+                'xxYEMNDAxHRUTSC')) AS BLP_DATE
+ 
+FROM MAIN M
+ 
+LEFT OUTER JOIN HOUSE_EVENTS HE
+        ON HE.SHPMNT_REF = M.SHPMNT_REF
+GROUP BY
+M.SHPMNT_REF
+,M.CONSOL_REF        
